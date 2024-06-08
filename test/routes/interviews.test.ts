24f4e46615c834c88fab6d6692e48bb24fadeee7
@zacbe <zacbegonzalez@gmail.com>
2024-06-09 00:10:37 +0200

    chore: Add validation for missing fields in user interview creation route

--- test/routes/interviews.test.ts
+++ test/routes/interviews.test.ts
@@ -1,4 +1,7 @@
+import { RowDataPacket } from 'mysql2'
 import * as TestPrep from '../testPreparation'
+import * as db from '../../src/logic/db'
+import { createUserInterview } from '../../src/logic/interviews'
 
 // Generate valid tokens at jwt.io with Algo HS256 and key 'abcdefg'
 const validToken =
@@ -12,6 +15,17 @@ afterEach(async () => {
 
 describe('User Interview Route', () => {
   describe('POST /userinterview', () => {
+    it('returns 400 if any field is missing', async () => {
+      const response = await TestPrep.sendPostRequest({
+        path: '/userinterview',
+        postBody: { availability: '2024-01-10' },
+        jwtToken: validToken,
+      })
+
+      expect(response.status).toBe(400)
+      expect(response.body.error.code).toBe('PARAMETER_INVALID')
+    })
+
     it('returns 201 if data is saved successfully', async () => {
       const body = {
         availability: '2024-01-10',
@@ -25,10 +39,66 @@ describe('User Interview Route', () => {
       })
 
       expect(response.status).toBe(201)
+      expect(response.body.interviewId).toBeDefined()
+
+      const { interviewId } = response.body
+
+      const [rows] = await db.pool.query<RowDataPacket[]>({
+        sql: `SELECT * 
+              FROM user_interviews 
+              WHERE id = :interviewId`,
+        values: { interviewId },
+      })
+      expect(rows).toHaveLength(1)
+      expect(rows[0].id).toBe(interviewId)
     })
   })
 
   describe('GET /userinterview', () => {
+    it('returns the latest user interviews with a specified limit', async () => {
+      const limit = 5
+      const records = 6
+      for (let i = 1; i <= records; i++) {
+        // eslint-disable-next-line no-await-in-loop
+        await createUserInterview({
+          availability: `2024-01-${i.toString().padStart(2, '0')}`,
+          phoneNumber: `123-456-78${i.toString().padStart(2, '0')}`,
+          birthdate: `2000-01-${i.toString().padStart(2, '0')}`,
+        })
+      }
+
+      const response = await TestPrep.getGetHttpResponse({
+        path: `/userinterview?limit=${limit}`,
+        jwtToken: validToken,
+      })
+
+      expect(response.status).toBe(200)
+      expect(response.body.payload.ids).toHaveLength(limit)
+      expect(response.body.meta.hasMore).toBe(true)
+    })
+
+    it('returns no more entries when limit exceeds total entries', async () => {
+      const limit = 5
+      const records = 4
+      for (let i = 1; i <= records; i++) {
+        // eslint-disable-next-line no-await-in-loop
+        await createUserInterview({
+          availability: `2024-01-${i.toString().padStart(2, '0')}`,
+          phoneNumber: `123-456-78${i.toString().padStart(2, '0')}`,
+          birthdate: `2000-01-${i.toString().padStart(2, '0')}`,
+        })
+      }
+
+      const response = await TestPrep.getGetHttpResponse({
+        path: `/userinterview?limit=${limit}`,
+        jwtToken: validToken,
+      })
+
+      expect(response.status).toBe(200)
+      expect(response.body.payload.ids).toHaveLength(records)
+      expect(response.body.meta.hasMore).toBe(false)
+    })
+
     it('returns an empty array if there are no user interviews', async () => {
       const response = await TestPrep.getGetHttpResponse({
         path: '/userinterview',
