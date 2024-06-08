55e96ca0b803fa3fc629d57c38fa4132a6a2b3ed
@zacbe <zacbegonzalez@gmail.com>
2024-06-08 22:56:22 +0200

    chore: introduced integration test

--- test/routes/chats.test.ts
+++ test/routes/chats.test.ts
@@ -1,5 +1,6 @@
-import { newChat } from '../../src/logic/chats'
+import { newChat, getNewestMessageId } from '../../src/logic/chats'
 import * as TestPrep from '../testPreparation'
+import * as db from '../../src/logic/db'
 
 // generate other valid tokens at jwt.io with Algo HS256 and key 'abcdefg'
 const validToken =
@@ -45,6 +46,55 @@ describe('Chat routes', () => {
 
       expect(response.status).toBe(400)
       expect(response.body.error.code).toBe('PARAMETER_INVALID')
+    })
+
+    it('returns 404 if chat with user is not found', async () => {
+      const targetUserId = 5
+      const response = await TestPrep.sendPostRequest({
+        path: '/deletenewestmessage',
+        postBody: { userId: targetUserId },
+        jwtToken: validToken,
+      })
+
+      expect(response.status).toBe(404)
+      expect(response.body.error.code).toBe('NOT_FOUND')
+    })
+
+    it('returns 404 if there are no messages to delete', async () => {
+      const targetUserId = 5
+      const response = await TestPrep.sendPostRequest({
+        path: '/deletenewestmessage',
+        postBody: { userId: targetUserId },
+        jwtToken: validToken,
+      })
+
+      expect(response.status).toBe(404)
+      expect(response.body.error.code).toBe('NOT_FOUND')
+    })
+
+    it('returns success if the newest message is deleted', async () => {
+      const targetUserId = 5
+      const message = 'Hello'
+
+      const chatId = await newChat({ userId, targetUserId: 5 })
+      // insert new message to chat
+      await db.pool.query(
+        'INSERT INTO chat_messages (chat_id, user_id, message) VALUES (?, ?, ?)',
+        [chatId, userId, message]
+      )
+
+      const response = await TestPrep.sendPostRequest({
+        path: '/deletenewestmessage',
+        postBody: { userId: targetUserId },
+        jwtToken: validToken,
+      })
+
+      expect(response.status).toBe(200)
+      expect(response.body.success).toBe(true)
+
+      // check if the message is deleted
+      const messageId = await getNewestMessageId(chatId)
+      expect(messageId).toBeNull()
     })
   })
 })
